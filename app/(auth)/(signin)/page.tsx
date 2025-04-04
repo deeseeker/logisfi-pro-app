f7708efbf2cc5d3ad65d2ca951216bedc7d51a71
Qudus Adeyemi <adeyemiqudus361@gmail.com>
2025-04-04 14:37:13 +0100

    api

--- app/(auth)/(signin)/page.tsx
+++ app/(auth)/(signin)/page.tsx
@@ -1,37 +1,37 @@
-import UserAuthForm from '@/components/forms/user-auth-form'
-import { Metadata } from 'next'
-import Image from 'next/image'
+import UserAuthForm from "@/components/forms/user-auth-form";
+import { Metadata } from "next";
+import Image from "next/image";
 
 export const metadata: Metadata = {
-  title: 'Authentication',
-  description: 'Authentication forms built using the components.'
-}
+  title: "Authentication",
+  description: "Authentication forms built using the components.",
+};
 export default function AuthenticationPage() {
   return (
-    <div className='min-h-screen flex items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0'>
+    <div className="min-h-screen flex items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
       <Image
-        src='/login-bg.jpeg'
-        alt='login bg'
+        src="/login-bg.jpeg"
+        alt="login bg"
         width={400}
         height={400}
-        className='object-cover h-full w-full hidden lg:block'
+        className="object-cover h-full w-full hidden lg:block"
         priority
       />
 
-      <div className='mx-auto w-full px-12 sm:px-0 sm:w-[350px]'>
-        <div className='flex flex-col text-center'>
-          <div className='flex justify-center mb-2'>
+      <div className="mx-auto w-full px-12 sm:px-0 sm:w-[350px]">
+        <div className="flex flex-col text-center">
+          <div className="flex justify-center mb-2">
             <Image
-              src='/logisfi-icon.svg'
+              src="/logisfi-icon.svg"
               width={121}
               height={30}
-              alt='logisfi pro icon'
+              alt="logisfi pro icon"
             />
           </div>
-          <p className='text-sm text-black'>Login to your account.</p>
+          <p className="text-sm text-black">Login to your account.</p>
         </div>
         <UserAuthForm />
       </div>
     </div>
-  )
+  );
 }
--- app/dashboard/history/[id]/page.tsx
+++ app/dashboard/history/[id]/page.tsx
@@ -1,38 +1,37 @@
-'use client'
-import AdminProfile from '@/components/profiles/admin-profile'
-import { Breadcrumbs } from '@/components/ui/breadcrumbs'
-import { Heading } from '@/components/ui/heading'
-import { Separator } from '@/components/ui/separator'
-import BankView from '@/components/view/bank-view-details'
-import { useEffect, useState } from 'react'
+"use client";
+import AdminProfile from "@/components/profiles/admin-profile";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import BankView from "@/components/view/bank-view-details";
+import { useEffect, useState } from "react";
 
 export default function TransactionId() {
-  const [role, setRole] = useState<string | null>(null)
+  const [role, setRole] = useState<string | null>(null);
 
   useEffect(() => {
     // Access localStorage only after the component has mounted
-    const userRole = localStorage.getItem('role')
-    setRole(userRole)
-  }, [])
+    const userRole = localStorage.getItem("role");
+    setRole(userRole);
+  }, []);
   const renderContent = () => {
     switch (role) {
-      case 'Admin':
-        return <AdminProfile />
-      case 'bank':
-        return <BankView />
+      case "Admin":
+        return <AdminProfile />;
+      case "bank":
+        return <BankView />;
 
       default:
-        break
+        break;
     }
-  }
+  };
   return (
-    <div className='space-y-2'>
+    <div className="space-y-2">
       <Heading
-        title='Transaction TH.0.01'
-        description='View transaction details'
+        title="Transaction TH.0.01"
+        description="View transaction details"
       />
       <Separator />
       {role ? renderContent() : <p>Loading...</p>}
     </div>
-  )
+  );
 }
